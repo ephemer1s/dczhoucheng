3bc0431d15b7367248382be6c7a46c04f0c6a37d
Spica TSUKIMI <62980323+ephemer1s@users.noreply.github.com>
2020-06-10 16:11:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import torch
 from torch import nn
@@ -6,9 +8,15 @@ from torch.utils.data import DataLoader, TensorDataset
 from scipy import stats
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
+import csv
 
 
 def featureparam(a):
+    """
+    根据信号特征值处理数据
+    :param a: numpy.ndarray
+    :return: torch.tensor
+    """
     x_avg = np.mean(a, axis=1, keepdims=True)  # 算术均值
     x_std = np.std(a, axis=1, ddof=1, keepdims=True)  # 标准差
     x_var = np.var(a, axis=1, ddof=1, keepdims=True)  # 方差
@@ -22,6 +30,11 @@ def featureparam(a):
 
 
 def fastft(a):
+    """
+    根据傅里叶变换处理特征值
+    :param a: numpy.ndarray
+    :return: (torch.tensor, torch.tensor)
+    """
     x = np.array([fft(a[i, :]) for i in range(a.shape[0])])
     y = np.array([np.abs(x[i, :]) for i in range(a.shape[0])])
     y = torch.from_numpy(y / (y.max() - y.min()))
@@ -30,29 +43,40 @@ def fastft(a):
     return y, z
 
 
+# 搭建1D-CNN网络
+net = nn.Sequential(
+    nn.Conv1d(1, 64, 3, 2, 1),
+    nn.BatchNorm1d(64),
+    nn.ReLU(),
 
-# 搭建BP网络
-net1 = nn.Sequential(
-    nn.Linear(7, 50),
+    nn.Conv1d(64, 32, 3, 2, 1),
+    nn.BatchNorm1d(32),
     nn.ReLU(),
-    nn.Linear(50, 200),
+    nn.MaxPool1d(3, 2),
+
+    nn.Conv1d(32, 16, 4, 3, 2),
+    nn.BatchNorm1d(16),
     nn.ReLU(),
-    nn.Linear(200, 50),
+
+    nn.Conv1d(16, 16, 4, 2, 2),
+    nn.BatchNorm1d(16),
     nn.ReLU(),
-    nn.Linear(50, 10)
+    nn.MaxPool1d(2, 2),
+
+    nn.Conv1d(16, 16, 4, 2, 2),
+    nn.BatchNorm1d(16),
+    nn.ReLU(),
+
+    nn.Linear(32, 1),
 )
-net2 = nn.Sequential(
-    nn.Linear(6000, 1000),
-    nn.ReLU(),
-    nn.Linear(1000, 200),
-    nn.ReLU(),
-    nn.Linear(200, 40),
-    nn.ReLU(),
-    nn.Linear(40, 10)
-)
+print(net)
 
 # 初始化网络参数
-optimizer = torch.optim.SGD(net2.parameters(), lr=0.03)  # 构建随机梯度下降优化器, lr为学习率
+batch_size = 64
+learning_rate = 0.02
+num_epoches = 20
+# 初始化训练参数
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)  # 构建随机梯度下降优化器, lr为学习率
 loss_func = torch.nn.CrossEntropyLoss()  # 构建交叉熵损失函数
 losses = []
 acces = []
@@ -65,24 +89,30 @@ label = torch.from_numpy(train_set[:, [-1]]).long()
 tseq = np.array(train_set[:, 1:-1])
 
 # 计算数据特征
-# feature, _ = fastft(tseq)
-# feature = featureparam(tseq)
-feature = torch.from_numpy(tseq)
-print(feature.size())
+feature, _ = fastft(tseq)
+
+# 转移到GPU
+feature = feature.cuda()
+label = label.cuda()
+net = net.cuda()
+
+# 定义数据加载器
 deal_set = TensorDataset(feature, label)
-train_data = DataLoader(dataset=deal_set, batch_size=64, shuffle=True, num_workers=0)
+train_data = DataLoader(dataset=deal_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
 # 循环执行epoch
-for e in range(50):
+torch.cuda.synchronize()
+start = time.time()
+for e in range(num_epoches):
     train_loss = 0
     train_acc = 0
-    net2.train()  # 网络开始训练
+    net.train()  # 网络开始训练
     for im, label in train_data:
-        im = Variable(im)
+        im = Variable(torch.unsqueeze(im, dim=1).float())
         label = Variable(label)
         # 前向传播
-        out = net2(im)
-        loss = loss_func(out, label.squeeze())
+        out = net(im)
+        loss = loss_func(out, label)
         # 反向传播
         optimizer.zero_grad()
         loss.backward()
@@ -99,8 +129,11 @@ for e in range(50):
           % (e, train_loss / len(train_data), train_acc / len(train_data)))
     losses.append(train_loss / len(train_data))
     acces.append(train_acc / len(train_data))
+torch.cuda.synchronize()
+end = time.time()
+print("running time: ", (end-start))
 
-# 绘制测试曲线
+# 绘制训练曲线
 fig = plt.figure()
 ax = fig.add_subplot(121)
 bx = fig.add_subplot(122)
@@ -109,20 +142,33 @@ ax.plot(np.arange(len(losses)), losses)
 bx.set_title('train acc')
 bx.plot(np.arange(len(acces)), acces)
 plt.show()
-plt.pause(0)
-exit(0)
 
 # 读入测试集
 test_set = np.loadtxt("test_remastered.csv", delimiter=",", dtype=("float32"), skiprows=1)
 tseq = np.array(test_set[:, 1:])
-feature = featureparam(tseq)
+feature, _ = fastft(tseq)
+feature = feature.cuda()
 deal_set = TensorDataset(feature)
-test_data = DataLoader(dataset=deal_set, batch_size=32, shuffle=False, num_workers=0)
-net1.eval()  # 将模型改为预测模式
+test_data = DataLoader(dataset=deal_set, batch_size=64, shuffle=False, num_workers=0)
+
+# 对测试集给出预测
+net.eval()  # 将模型改为预测模式
 res = []
 for im in test_data:
-    im = Variable(im[0])
-    out = net1(im)
+    im = Variable(torch.unsqueeze(im[0], dim=1))
+    out = net(im)
     _, pred = out.max(1)
+    pred.unsqueeze_(1)
     res.append(pred)
+res = torch.cat(res, dim=0)
+res = res.cpu().numpy().squeeze()
 print(res)
+
+# 将结果保存到pred.csv
+headers = ['id', 'label']
+rows = np.arange(1, 529)
+rows = np.vstack((rows, res)).transpose(1, 0)
+with open('pred.csv', 'w', newline='') as f:
+    f_csv = csv.writer(f)
+    f_csv.writerow(headers)
+    f_csv.writerows(rows)
